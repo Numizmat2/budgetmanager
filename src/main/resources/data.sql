483cf536a7c7ec5751b91cdddd331b324abba178
Bartosz Pingot <bartoszpingot13@wp.pl>
2021-09-11 21:10:13 +0200

    Prepare database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+
+DROP TABLE IF EXISTS register;
+
+CREATE TABLE register (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(100) NOT NULL UNIQUE,
+  value VARCHAR(20) NOT NULL,
+  timestamp TIMESTAMP(3) NOT NULL
+);
+
+ALTER TABLE register ADD CONSTRAINT name_timestamp_unique UNIQUE (name, timestamp);
+
+INSERT INTO register (name, value, timestamp) VALUES
+('wallet', '1000', CURRENT_TIMESTAMP),
+('savings', '5000', CURRENT_TIMESTAMP),
+('insurance policy', '0', CURRENT_TIMESTAMP),
+('food expenses', '0', CURRENT_TIMESTAMP);
